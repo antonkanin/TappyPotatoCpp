78d1ebb856e061817a6f04b91d783dd1682a906b
Anton Kanin <anton.kanin@ephl.by>
2020-12-08 19:02:01 +0300

    replacing gl.h with glad.h; rendering a simple rect

--- VideoSystem.cpp
+++ VideoSystem.cpp
@@ -6,9 +6,10 @@
 
 #include <iostream>
 #include <string>
+#include <cassert>
 
-#include <GL/gl.h>
 #include <SDL2/SDL.h>
+#include <glad/glad.h>
 
 #include "Exceptions.hpp"
 
@@ -18,6 +19,33 @@ namespace tp
 // FIXME(Anton) move these to pimpl
 SDL_GLContext context_{};
 SDL_Window*   window_{};
+
+#define GL_CHECK()                                                                                 \
+    {                                                                                              \
+        const int err = static_cast<int>(glGetError());                                            \
+        if (err != GL_NO_ERROR)                                                                    \
+        {                                                                                          \
+            switch (err)                                                                           \
+            {                                                                                      \
+                case GL_INVALID_ENUM:                                                              \
+                    std::cerr << "GL_INVALID_ENUM" << std::endl;                                   \
+                    break;                                                                         \
+                case GL_INVALID_VALUE:                                                             \
+                    std::cerr << "GL_INVALID_VALUE" << std::endl;                                  \
+                    break;                                                                         \
+                case GL_INVALID_OPERATION:                                                         \
+                    std::cerr << "GL_INVALID_OPERATION" << std::endl;                              \
+                    break;                                                                         \
+                case GL_INVALID_FRAMEBUFFER_OPERATION:                                             \
+                    std::cerr << "GL_INVALID_FRAMEBUFFER_OPERATION" << std::endl;                  \
+                    break;                                                                         \
+                case GL_OUT_OF_MEMORY:                                                             \
+                    std::cerr << "GL_OUT_OF_MEMORY" << std::endl;                                  \
+                    break;                                                                         \
+            }                                                                                      \
+            assert(false);                                                                         \
+        }                                                                                          \
+    }
 
 const char* vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
@@ -31,8 +59,8 @@ const char* fragmentShaderSource = "#version 330 core\n"
                                    "uniform vec4 ourColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   // "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                   "   FragColor = ourColor;\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   // "   FragColor = ourColor;\n"
                                    "}\n\0";
 
 void VideoSystem::init() noexcept(false)
@@ -40,45 +68,157 @@ void VideoSystem::init() noexcept(false)
     if (SDL_InitSubSystem(SDL_INIT_VIDEO) != 0)
         throw Exception("Could not initialize SDL2 Video System " + std::string(SDL_GetError()));
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
-    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
     const int   SCREEN_WIDTH  = 800;
     const int   SCREEN_HEIGHT = 600;
     const char* SCREEN_TITLE  = "Tappy Potato";
 
     window_ = SDL_CreateWindow(SCREEN_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
-
     if (!window_)
         throw Exception("Could not create window: " + std::string(SDL_GetError()));
 
+    if (0 != SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG))
+        throw Exception("Could not set "
+                        "SDL_GL_CONTEXT_FLAGS: " +
+                        std::string(SDL_GetError()));
+
+    if (0 != SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3))
+        throw Exception(
+            "Could not set SDL_GL_CONTEXT_MAJOR_VERSION: " + std::string(SDL_GetError()));
+
+    if (0 != SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2))
+        throw Exception(
+            "Could not set SDL_GL_CONTEXT_MINOR_VERSION: " + std::string(SDL_GetError()));
+
+    if (0 != SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES))
+        throw Exception(
+            "Could not set SDL_GL_CONTEXT_PROFILE_MASK: " + std::string(SDL_GetError()));
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
     context_ = SDL_GL_CreateContext(window_);
+    if (!context_)
+        throw Exception("Could not create OpenGl context: " + std::string(SDL_GetError()));
 
-    // typedef
-    // initializeGLFunction();
+    //// Check OpenGL version
+    int glMajorVersion{};
+    int glMinorVersion{};
 
-    // void* func_pointer = SDL_GL_GetProcAddress("glCreateShader");
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &glMajorVersion);
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &glMinorVersion);
 
-    // typedef unsigned int GLenum;
+    std::cout << "OpenGL version " << glMajorVersion << '.' << glMinorVersion << std::endl;
 
-    // typedef void(glFuncPtr_t)(GLenum cap);
-    // auto glEnable = reinterpret_cast<glFuncPtr_t*>(func_pointer);
-
-    // if (glEnable == nullptr)
-    //     throw Exception("Could not initialize glCreateShader");
+    if (0 == gladLoadGLES2Loader(SDL_GL_GetProcAddress))
+        throw Exception("Could not initialize glad");
 
     glEnable(GL_DEPTH_TEST);
-    std::cout << "Error: " << glGetError() << std::endl;
 
-    // SDL_LoadFunction()
+    //// Initialize GL data
+    initializeVAO();
+
+    shaderProgram_ = glCreateProgram();
+    linkShaderProgram(shaderProgram_);
+}
+
+GLuint VideoSystem::createShader(const char*& shaderSourceCode, int shaderType) const
+{
+    GLuint shaderId = glCreateShader(shaderType);
+    glShaderSource(shaderId, 1, &shaderSourceCode, nullptr);
+    glCompileShader(shaderId);
+
+    int success{};
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        const int logSize = 512;
+        char      infoLog[logSize];
+        glGetShaderInfoLog(shaderId, logSize, nullptr, infoLog);
+        throw Exception("Shader error: " + std::string(infoLog));
+    }
+
+    return shaderId;
+}
+void VideoSystem::linkShaderProgram(GLuint_t shaderProgram_) noexcept(false)
+{
+    auto vertexShader   = createShader(vertexShaderSource, GL_VERTEX_SHADER);
+    auto fragmentShader = createShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
+
+    glAttachShader(shaderProgram_, vertexShader);
+    glAttachShader(shaderProgram_, fragmentShader);
+    glLinkProgram(shaderProgram_);
+
+    {
+        int success{};
+        glGetProgramiv(shaderProgram_, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            const int logSize = 512;
+            char      infoLog[logSize];
+            glGetProgramInfoLog(shaderProgram_, logSize, nullptr, infoLog);
+            throw Exception("Shader linking error: " + std::string(infoLog));
+        }
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
 
 void VideoSystem::render()
 {
-    SDL_Delay(2000);
+    glUseProgram(shaderProgram_);
+    GL_CHECK()
+
+    glBindVertexArray(VAO_);
+    GL_CHECK()
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    GL_CHECK()
+
+    glBindVertexArray(0);
+    GL_CHECK()
+
+    // SDL_Delay(2000);
+
+    SDL_GL_SwapWindow(window_);
+}
+
+void VideoSystem::initializeVAO()
+{
+    // clang-format off
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+    // clang-format on
+
+    glGenVertexArrays(1, &VAO_);
+    glBindVertexArray(VAO_);
+
+    GLuint VBO{};
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint EBO{};
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
+    glEnableVertexAttribArray(0);
+}
+
+VideoSystem::~VideoSystem()
+{
+    SDL_Quit();
 }
 
 } // namespace tp
--- VideoSystem.hpp
+++ VideoSystem.hpp
@@ -1,14 +1,25 @@
 #pragma once
 
+typedef unsigned int GLuint_t;
+
 namespace tp
 {
 class VideoSystem final
 {
 public:
+    ~VideoSystem();
     void init() noexcept(false);
     void render();
 
 private:
+    GLuint_t shaderProgram_{};
+    GLuint_t VAO_{};
+
+    unsigned int createShader(const char*& shaderSourceCode, int shaderType) const;
+    void linkShaderProgram(GLuint_t shaderProgram_) noexcept(false);
+
+    void initializeVAO();
+
 };
 
 } // namespace tp
