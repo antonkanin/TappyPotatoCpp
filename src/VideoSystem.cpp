36972b0b8a3a304633ae0eaa4910626bfb426775
Anton Kanin <anton.kanin@ephl.by>
2020-12-11 18:02:39 +0300

    updating glsl version to 300 es

--- src/VideoSystem.cpp
+++ src/VideoSystem.cpp
@@ -47,16 +47,16 @@ SDL_Window*   window_{};
         }                                                                                          \
     }
 
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = "#version 300 es\n"
                                  "layout (location = 0) in vec3 aPos;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
                                  "}\0";
 
-const char* fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "uniform vec4 ourColor;\n"
+const char* fragmentShaderSource = "#version 300 es\n"
+                                   "out lowp vec4 FragColor;\n"
+                                   // "uniform vec4 ourColor;\n"
                                    "void main()\n"
                                    "{\n"
                                    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
